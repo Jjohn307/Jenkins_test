569c32ae05d9199763c55772298e6be8b6cbb6eb
developer <sjalee82@gmail.com>
2019-12-11 11:12:19 -0500

    API added

---
+++ src/main/java/com/mkyong/hashing/Greetings_Controller.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greetings_Controller()
+{
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+  @RequestMapping("/Hello")
+  public Greetings greetings(@RequestParam(value="name", defaultValue="World") String name)
+  {
+  		return new Greeting(counter.incrementAndGet(),
+              String.format(template, name));
+  }
+}
